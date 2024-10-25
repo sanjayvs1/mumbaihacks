ed1c735dda080b0ee2b97d81b34e8a785a42b723
dipesh-m12 <mavinash423@gmail.com>
2024-10-25 20:05:24 +0530

    public

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

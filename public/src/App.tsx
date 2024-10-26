e9d545c684670e658b6bbacd399e58eba9d6370f
Krishna Agrawal <krishnaagrawal2304@gmail.com>
2024-10-26 11:27:51 +0530

    HomePage added

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/HomePage" element={<HomePage />} /> 
       </Routes>
     </BrowserRouter>
   );

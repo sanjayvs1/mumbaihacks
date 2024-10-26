543cbbd8fc9ff2f9b35f1b8bcc0c0a76c295c819
pradeepDu <dukemarquis2004@gmail.com>
2024-10-26 11:36:09 +0530

    Web3 Login page

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Meetings from "./pages/Meetings";
 import Meeting from "./pages/Meeting";
+import Web3Login from "./pages/Web3Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Web3Login />} /> {/* Added Web3Login route */}
         <Route path="/meeting/:filename" element={<Meeting />} />
         <Route path="/meetings" element={<Meetings />} />
       </Routes>
@@ -17,3 +19,4 @@ function App() {
 }
 
 export default App;
+
